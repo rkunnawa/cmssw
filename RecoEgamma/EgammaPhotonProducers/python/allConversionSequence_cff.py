55b581c2a8df75ca278395204be100d708547236
Yetkin <yetkin.yilmaz@cern.ch>
2013-09-05 16:02:42 +0200

    changes added

--- RecoEgamma/EgammaPhotonProducers/python/allConversionSequence_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-#
-#
-# Tracker only conversion producer
-from RecoEgamma.EgammaPhotonProducers.allConversions_cfi import *
-allConversionSequence = cms.Sequence(allConversions)
-
-
