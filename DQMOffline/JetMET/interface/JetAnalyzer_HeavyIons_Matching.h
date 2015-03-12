1bff9efb6a8e91c93b5b8627f88d6c452547b77e
Raghav Kunnawalkam Elayavalli <raghav.k.e@cern.ch>
2015-03-12 02:22:09 +0100

    first commit for staring the delta R matching

---
+++ DQMOffline/JetMET/interface/JetAnalyzer_HeavyIons_Matching.h
@@ -0,0 +1,15 @@
+#ifndef JetAnalyzer_HeavyIons_Matching_H
+#define JetAnalyzer_HeavyIons_Matching_H
+
+
+//
+// Jet Tester class for heavy ion jets. for DQM jet analysis monitoring 
+// For CMSSW_7_4_X, especially reading background subtracted jets 
+// author: Raghav Kunnawalkam Elayavalli, Mohammed Zakaria
+//         Jan 12th 2015 
+//         Rutgers University, email: raghav.k.e at CERN dot CH 
+//
+// Setup class for delta R matching in heavyions for Vs - Pu, Calo - PF along with reco-gen matching.   
+//
+
+
---
+++ DQMOffline/JetMET/src/JetAnalyzer_HeavyIons_Matching.cc
@@ -0,0 +1,28 @@
+//
+// Jet Analyzer class for heavy ion jets. for DQM jet analysis monitoring 
+// For CMSSW_7_4_X, especially reading background subtracted jets 
+// author: Raghav Kunnawalkam Elayavalli, Mohammed Zakaria
+//         Jan 12th 2015 
+//         Rutgers University, email: raghav.k.e at CERN dot CH 
+//
+
+
+#include "DQMOffline/JetMET/interface/JetAnalyzer_HeavyIons_matching.h"
+
+
+using namespace edm;
+using namespace reco;
+using namespace std;
+
+// declare the constructors:
+JetAnalyzer_HeavyIons_matching::JetAnalyzer_HeavyIons_matching() :
+
+  
+
+{
+
+
+
+}
+
+
